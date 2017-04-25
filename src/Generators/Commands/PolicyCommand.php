eb91eb003a36c5f07cf48957d4b2399be61ec91b
djso(소두진) <djso@saramin.co.kr>
2017-04-25 17:23:29 +0900

    policy command 추가

---
+++ src/Generators/Commands/PolicyCommand.php
@@ -0,0 +1,71 @@
+<?php
+namespace Visualplus\Larabase\Generators\Commands;
+
+use Illuminate\Console\Command;
+use Visualplus\Larabase\Generators\FileAlreadyExistsException;
+use Symfony\Component\Console\Input\InputArgument;
+use Visualplus\Larabase\Generators\PolicyGenerator;
+
+class PolicyCommand extends Command
+{
+
+    /**
+     * The name of command.
+     *
+     * @var string
+     */
+    protected $name = 'gen:policy';
+
+    /**
+     * The description of command.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new action.';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Policy';
+
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+
+        try {
+            (new PolicyGenerator([
+                'name'  => $this->argument('name'),
+            ]))->run();
+            $this->info($this->type . " created successfully.");
+
+        } catch (FileAlreadyExistsException $e) {
+            $this->error($this->type . ' already exists!');
+
+            return false;
+        }
+    }
+
+    /**
+     * The array of command arguments.
+     *
+     * @return array
+     */
+    public function getArguments()
+    {
+        return [
+            [
+                'name',
+                InputArgument::REQUIRED,
+                'The name of model for which the presenter is being generated.',
+                null
+            ],
+        ];
+    }
+}
